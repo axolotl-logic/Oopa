f9d4011d87febd8d845ec1c13b218b0ac7cf39e9
Autumn Green <me@axolotl-logic.io>
2025-02-28 15:11:33 -0500

    docs: add markdown content type

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="oopa",
-    version="2.0",
+    version="2.0.1",
     description="A script that dumps stats on a wordlist for password research",
     author="Axolotl-Logic",
     author_email="me@axolotl-logic.io",
@@ -11,4 +11,6 @@ setup(
     install_requires=["prettytable"],
     url="https://www.axolotl-logic.io/",
     long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    license="MIT",
 )
